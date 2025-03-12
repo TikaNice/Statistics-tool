e58cd4bf20271cf46ce14200f8b632c7bbff96d3
Jinpeng Deng <113403050+TikaNice@users.noreply.github.com>
2025-03-11 20:58:00 -0400

    Update Q1_Q3_IQR_outliers_mean_standard_deviation_and_percent.py

--- api/Q1_Q3_IQR_outliers_mean_standard_deviation_and_percent.py
+++ api/Q1_Q3_IQR_outliers_mean_standard_deviation_and_percent.py
@@ -24,21 +24,21 @@ def handler(request):
             result = Q1_Q3_IQR_outliers_mean_and_standard_devisition(data_list)
 
             # 处理百分比逻辑
-            if "percent" in data:
-                requir_percent = data["percent"]
-                try:
-                    if requir_percent.endswith('%'):
-                        requir_num = float(requir_percent[:-1])
-                    else:
-                        requir_num = float(requir_percent) * 100
+            
+            requir_percent = data["percent"]
+            try:
+                if requir_percent.endswith('%'):
+                    requir_num = float(requir_percent[:-1])
+                else:
+                    requir_num = float(requir_percent) * 100
 
-                    if 0 <= requir_num <= 100:
-                        percent = np.percentile(data_list, requir_num)
-                        result["percent"] = percent
-                    else:
-                        result["percent"] = "Value out of range (0-100)."
-                except ValueError:
-                    result["percent"] = "Invalid value for percent."
+                if 0 <= requir_num <= 100:
+                    percent = np.percentile(data_list, requir_num)
+                    result["percent"] = percent
+                else:
+                    result["percent"] = "Value out of range (0-100)."
+            except ValueError:
+                result["percent"] = "Invalid value for percent."
 
             return Response(
                 json.dumps(result),
